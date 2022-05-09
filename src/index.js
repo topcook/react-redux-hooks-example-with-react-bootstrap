3bd113b8cc8a5eeac7b2715bd1cd693f2112c2ff
topcook <codecooker1996@gmail.com>
2022-05-09 13:10:21 -0400

    added responsive by using react-bootstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const store = createStore(rootReducer)
 
--- src/views/SimpleCounter.js
+++ src/views/SimpleCounter.js
@@ -1,39 +1,52 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { CountAction } from '../action/CountAction';
+import { Col, Row } from 'react-bootstrap';
 
 const SimpleCounter = () => {
   //Get counter from counterReducer
   const { handleClick } = CountAction()
   const counter = useSelector(state => state.myCounter.counter);
 
-    return (         
-        <section id="section-counter">
-        <h3 className="center-align white-text blue">Simple Counter</h3>
-        <h5 className="center-align">{counter}</h5>
-        <div className="row counter-btns">
-        <button className="btn-small blue" onClick={() => handleClick('DOUBLE_COUNT')}>
-        x2
-        </button>
-        <button className="btn-small blue" onClick={() => handleClick('INCREMENT_COUNT_TEN')}>
-        +10
-        </button>
-        <button className="btn-small blue" onClick={() => handleClick('INCREMENT_COUNT')}>
-        +1
-        </button>
-        <button className="btn-small blue" onClick={() => handleClick('DECREMENT_COUNT')}>
-        -1
-        </button>
-        <button className="btn-small blue" onClick={() => handleClick('DECREMENT_COUNT_TEN')}>
-        -10
-        </button>
-        <button className="btn-small blue" onClick={() => handleClick('RESET_COUNT')}>
-        Reset
-        </button>
-        </div>
-     
-        </section>
-     );
+  return (
+    <section id="section-counter">
+      <h3 className="center-align white-text blue">Simple Counter</h3>
+      <h5 className="center-align">{counter}</h5>
+      <div className="row counter-btns">
+        <Col xs={4} sm={2} className="mb-2">
+          <button className="btn-small blue w-100" onClick={() => handleClick('DOUBLE_COUNT')}>
+            x2
+          </button>
+        </Col>
+        <Col xs={4} sm={2} className="mb-2">
+          <button className="btn-small blue w-100" onClick={() => handleClick('INCREMENT_COUNT_TEN')}>
+            +10
+          </button>
+        </Col>
+        <Col xs={4} sm={2} className="mb-2">
+          <button className="btn-small blue w-100" onClick={() => handleClick('INCREMENT_COUNT')}>
+            +1
+          </button>
+        </Col>
+        <Col xs={4} sm={2} className="mb-2">
+          <button className="btn-small blue w-100" onClick={() => handleClick('DECREMENT_COUNT')}>
+            -1
+          </button>
+        </Col>
+        <Col xs={4} sm={2} className="mb-2">
+          <button className="btn-small blue w-100" onClick={() => handleClick('DECREMENT_COUNT_TEN')}>
+            -10
+          </button>
+        </Col>
+        <Col xs={4} sm={2} className="mb-2">
+          <button className="btn-small blue w-100" onClick={() => handleClick('RESET_COUNT')}>
+            Reset
+          </button>
+        </Col>
+      </div>
+
+    </section>
+  );
 }
- 
+
 export default SimpleCounter;
